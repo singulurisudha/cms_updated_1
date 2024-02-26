ff5cbebc4a4d880106f660a0b961811f16226ea4
S Sudha <89729299+singulurisudha@users.noreply.github.com>
2024-02-26 20:24:20 +0530

    Update serializers.py

--- user/serializers.py
+++ user/serializers.py
@@ -7,11 +7,11 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-<<<<<<< HEAD
+
         fields = ('username', 'email', 'password', 'is_staff')
-=======
+
         fields = ('username', 'email', 'password','is_staff')
->>>>>>> 1103a6d72f00a37c7610cfaf2dd1621e0293f610
+
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -22,23 +22,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             user = User.objects.create_user(**validated_data)
             return user
 
-<<<<<<< HEAD
-    
 
-
-=======
->>>>>>> 1103a6d72f00a37c7610cfaf2dd1621e0293f610
 class UserLoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField()
 
-<<<<<<< HEAD
 
-
-
-=======
 class ForgetPasswordUpdateSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
->>>>>>> 1103a6d72f00a37c7610cfaf2dd1621e0293f610
+
 
